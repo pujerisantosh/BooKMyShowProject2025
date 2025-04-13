d8164fe47940b865be51fdf6458ccda716ca69c7
Santosh Pujeri <16santoshpujari@gmail.com>
2025-04-13 16:13:25 +0530

    BOOKMYSHOWPROJECT-2025

---
+++ src/main/java/com/scaler/bookmyshowproject2025/BookMyShowProject2025Application.java
@@ -0,0 +1,13 @@
+package com.scaler.bookmyshowproject2025;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookMyShowProject2025Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookMyShowProject2025Application.class, args);
+    }
+
+}
